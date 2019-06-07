4627e0fcb762f4ab2bcbe7ae3b8ff3196bbaa758
zyemt <39991355+zyemt@users.noreply.github.com>
2019-06-07 22:36:42 +0800

    Update knn.py

--- knn.py
+++ knn.py
@@ -11,18 +11,18 @@ def etc(x,y):
 def findclosest(x,x_train,y_train,k):
     count1 ={}
     grp = []
-    for i in range(len(x_train)):
+    for i in range(len(x_train)):#对距离排序
         dist = etc(x,x_train[i])
         grp.append(dist)
     grp = np.array(grp)
-    sortedDistance = grp.argsort()
+    sortedDistance = grp.argsort()#根据大小获得列表元素下标的排序
 
     for i in range(k):
         if y_train[sortedDistance[i]] in count1:
             count1[y_train[sortedDistance[i]]] += 1
         else:
             count1[y_train[sortedDistance[i]]] = 1
-    count2 = sorted(count1.items(),key = operator.itemgetter(1),reverse = True)
+    count2 = sorted(count1.items(),key = operator.itemgetter(1),reverse = True)#分类
     return count2[0][0]
 
 def calculate(test_data,train_data,y_train,k):
@@ -38,6 +38,6 @@ def final():
     y = iris.target
     x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.5)#x_train 数据特征 y_train 数据得分类
     prediction = calculate(x_test,x_train,y_train,k)
-    print(accuracy_score(y_test,prediction))
+    print(accuracy_score(y_test,prediction))#计算准确率
 
 final()
